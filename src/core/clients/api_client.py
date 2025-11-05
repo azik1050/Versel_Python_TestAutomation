47df5ff3d14e827d689d652f536772b20bddeef8
Azik1050 <azik1506@mail.ru>
2025-11-05 20:05:56 +0500

    fixes

--- src/core/clients/api_client.py
+++ src/core/clients/api_client.py
@@ -9,6 +9,7 @@ from requests import Response
 class ApiClient:
     def __init__(self, base_url: str):
         self._base_url = base_url
+        self.__session = requests.Session()
 
     @staticmethod
     def __request_logging():
@@ -33,54 +34,58 @@ class ApiClient:
         return decorator
 
     @__request_logging()
-    def _get(self, path: str, params=None) -> Response:
+    def _get(self, path: str, headers: dict = None, params=None) -> Response:
         allure.attach(
             f"PUT {path}. Params : {params}",
             name="Request information",
             attachment_type=allure.attachment_type.TEXT
         )
-        response = requests.get(
+        response = self.__session.get(
             url=self._base_url + path,
+            headers=headers,
             params=params
         )
         return response
 
     @__request_logging()
-    def _post(self, path: str, json, params=None) -> Response:
+    def _post(self, path: str, json, headers: dict = None, params=None) -> Response:
         allure.attach(
             f"POST {path}. JSON: {json}. Params : {params}",
             name="Request information",
             attachment_type=allure.attachment_type.TEXT
         )
-        response = requests.post(
+        response = self.__session.post(
             url=self._base_url + path,
             json=json,
+            headers=headers,
             params=params
         )
         return response
 
     @__request_logging()
-    def _put(self, path: str, json, params=None) -> Response:
+    def _put(self, path: str, json, headers: dict = None, params=None) -> Response:
         allure.attach(
             f"PUT {path}. JSON: {json}. Params : {params}",
             name="Request information",
             attachment_type=allure.attachment_type.TEXT
         )
-        return requests.put(
+        return self.__session.put(
             url=self._base_url + path,
             json=json,
+            headers=headers,
             params=params
         )
 
     @__request_logging()
-    def _delete(self, path: str, params=None) -> Response:
+    def _delete(self, path: str, headers: dict = None, params=None) -> Response:
         allure.attach(
             f"DELETE {path}. Params : {params}",
             name="Request information",
             attachment_type=allure.attachment_type.TEXT
         )
-        response = requests.delete(
+        response = self.__session.delete(
             url=self._base_url + path,
+            headers=headers,
             params=params
         )
         return response
