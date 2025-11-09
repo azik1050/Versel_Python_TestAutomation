992b14a8d356f2316e791ce14f43340014cb5e4f
Azik1050 <azik1506@mail.ru>
2025-11-10 02:07:58 +0500

    Implicit wait fix added

--- src/core/driver/driver_factory.py
+++ src/core/driver/driver_factory.py
@@ -6,7 +6,7 @@ from src.core.utils.enums.Browser import Browser
 
 
 class DriverFactory:
-    DEFAULT_AWAIT_TIME = TestConfig.IMPLICIT_DRIVER_WAIT
+    DEFAULT_AWAIT_TIME = TestConfig.UI.IMPLICIT_DRIVER_WAIT
 
     @classmethod
     def create_driver(cls, browser: Browser) -> WebDriver | None:
