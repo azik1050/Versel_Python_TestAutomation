3ffb8b91d928be3372271157ed5d145f56b6fb90
Azik1050 <azik1506@mail.ru>
2025-11-11 05:18:47 +0500

    ApiAssertions refactored; common logic consolidated

--- src/applications/jira/utils/api_assertions.py
+++ src/applications/jira/utils/api_assertions.py
@@ -9,39 +9,35 @@ from src.core.utils.allure import attach_api_call
 
 class ApiAssertions:
     @staticmethod
-    def _attach_response(response: Response):
-        allure.attach(
-            str(response.status_code),
-            name="Response Code",
-            attachment_type=allure.attachment_type.TEXT
-        )
-        allure.attach(
-            str(response.text),
-            name="Response Body",
-            attachment_type=allure.attachment_type.TEXT
-        )
+    def _attach_and_raise(error_text: str, response: Response, e: Exception) -> None:
+        logger.error(f"{error_text}. Response code: {response.status_code}. Response body: {response.text}")
+        attach_api_call(response)
+        raise AssertionError(f"Invalid response code. Error message: {e}")
 
     @staticmethod
     def assert_status_code(response: Response, expected_code: int):
-        error_message = f"Actual code: {response.status_code}; Expected: {expected_code}"
-        ApiAssertions._attach_response(response)
         try:
             with allure.step("Verify response code"):
-                assert response.status_code == expected_code, error_message
-                logger.debug(f"Valid received response for {response.url}: {response.text}")
+                assert response.status_code == expected_code, f"Actual code: {response.status_code}; Expected: {expected_code}"
+                allure.attach(
+                    str(response.status_code),
+                    name="Response Code",
+                    attachment_type=allure.attachment_type.TEXT
+                )
+                logger.debug(f"Valid response code for: {response.url} | {response.status_code}")
         except AssertionError as e:
-            logger.error(f"Invalid response code: {response.status_code}. Response body: {response.text}")
-            attach_api_call(response)
-            raise AssertionError(f"Invalid response code. Error message: {e}")
+            ApiAssertions._attach_and_raise("Invalid Response Code", response, e)
 
     @staticmethod
     def validate_response_model(response: Response, expected_model: type[BaseModel]):
-        # ApiAssertions._attach_response(response)
         try:
             with allure.step("Verify response body model"):
                 expected_model.model_validate(response.json())
-                logger.debug(f"Valid response model for {response.url}: {response.text}")
+                allure.attach(
+                    response.text,
+                    name="Response Body",
+                    attachment_type=allure.attachment_type.JSON
+                )
+                logger.debug(f"Valid response model for {response.url} | {response.text}")
         except ValidationError as e:
-            logger.error(f"Invalid response body, response body: {response.text}. Error: {e}")
-            attach_api_call(response)
-            raise AssertionError(f"Invalid response schema from API. Error: {e}")
+            ApiAssertions._attach_and_raise("Invalid Response Body", response, e)
