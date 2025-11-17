002c833aab8ccc5915613b8b08b25640bde2db0e
Azik1050 <azik1506@mail.ru>
2025-11-18 01:44:50 +0500

    githib actions CI done

--- src/test/api/base_test.py
+++ src/test/api/base_test.py
@@ -6,5 +6,12 @@ from pytest import mark
 
 @mark.api
 class BaseTest(ApiAssertions):
+    def __init__(self):
+        self.__soft_asserts = []
+
+    def soft_assert(self, condition: bool, message: str):
+        if not condition:
+            self.__soft_asserts.append(message)
+
     def step(self, message: str):
         return allure.step(message)
